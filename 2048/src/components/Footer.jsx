8df7fbff8c65fa085448cf39fb9de712c85e7a83
ThiBrule <t_brule1@hetic.eu>
2025-12-08 09:18:55 +0100

    Ajout nouveaux déplacements.

--- 2048/src/components/Footer.jsx
+++ 2048/src/components/Footer.jsx
@@ -2,11 +2,11 @@ export default function Footer() {
   return (
     <footer className="footer">
       <h3>Commandes du jeu</h3>
-      <p>⬆️ Flèche Haut : déplacer vers le haut</p>
-      <p>⬇️ Flèche Bas : déplacer vers le bas</p>
-      <p>⬅️ Flèche Gauche : déplacer à gauche</p>
-      <p>➡️ Flèche Droite : déplacer à droite</p>
-      <p>Si vous souhaitez en savoir plus découvrez notre Github.</p>
+      <p>⬆️ Flèche Haut ou Z : déplacer vers le haut</p>
+      <p>⬇️ Flèche Bas ou S : déplacer vers le bas</p>
+      <p>⬅️ Flèche Gauche ou Q : déplacer à gauche</p>
+      <p>➡️ Flèche Droite ou D : déplacer à droite</p>
+      <p>Si vous souhaitez en savoir plus découvrez notre Github : https://github.com/ThiBrule.</p>
     </footer>
   );
 }
